e228e66adf6e760513cd3d1f58fce6393cb972ec
Cooper Fryar <cooperfryar@gmail.com>
2023-08-21 16:33:37 -0500

    Task E3: Playtime 1-3

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "adding unique products to cart" do
+    cart = carts(:one)
+    product_1 = products(:one)
+    product_2 = products(:two)
+
+    cart.add_product(product_1)
+    cart.add_product(product_2)
+
+    assert_equal 2, cart.line_items.size
+    assert_equal product_1.price + product_2.price, cart.total_price
+  end
+
+  test "adding duplicate product to cart" do
+    cart = Cart.new
+
+    product = products(:ruby)
+
+    cart.add_product(product)
+    cart.add_product(product)
+
+    expected_total_price = product.price + product.price
+    assert_equal 2, cart.line_items.size
+    assert_equal expected_total_price, cart.total_price
+  end
 end
